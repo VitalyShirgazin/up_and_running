5dc1504c5cd075282fc56e5cc757651d7e9b2122
Vitaly Shirgazin <kukish6000@gmail.com>
2024-05-29 03:24:44 -0400

    Add isolation_file_layouts set

---
+++ isolation_file_layouts/stage/data-stores/mysql/main.tf
@@ -0,0 +1,29 @@
+provider "aws" { 
+    region = "us-east-1"
+}
+resource "aws_db_instance" "example" {
+    identifier_prefix   = "up-and-running"
+    engine              = "mysql"
+    allocated_storage   = 10
+    instance_class      = "db.t3.micro"
+    skip_final_snapshot = true
+    db_name             = "example_database"
+    # How should we set the username and password?
+    username            = var.db_username
+    password            = var.db_password
+}
+# !!!!!!!!!!!!!!!How to set the username and password?!!!!!!!!!!!!!!!!
+# export TF_VAR_db_username="(YOUR_DB_USERNAME)"
+# export TF_VAR_db_password="(YOUR_DB_PASSWORD)"
+
+terraform {
+    backend "s3" {
+      bucket = "funny-terraform-bucket"
+      key    = "stage/data-stores/mysql/terraform.tfstate"
+      region = "us-east-1"
+      # Replace this with your DynamoDB table name!
+      dynamodb_table = "funny-terraform-bucket-locks"
+      encrypt = true 
+   }
+}
+
---
+++ isolation_file_layouts/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,158 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-"
+  image_id      = "ami-0bb84b8ffd87024d8"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "Hello, world!" > /var/www/html/index.html
+    echo "${data.terraform_remote_state.db.outputs.address}" >> /var/www/html/index.html
+    echo "${data.terraform_remote_state.db.outputs.port}" >> /var/www/html/index.html
+    EOF
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_autoscaling_group" "example" {
+  desired_capacity     = 2
+  min_size             = 2
+  max_size             = 10
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = var.instance_security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = var.alb_name
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = var.alb_name
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = var.alb_security_group_name
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "terraform_remote_state" "db" {
+    backend = "s3"
+    config = {
+      bucket = "funny-terraform-bucket"
+      key    = "stage/data-stores/mysql/terraform.tfstate"
+      region = "us-east-1"
+  } 
+}
